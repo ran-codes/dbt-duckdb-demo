1c33fbc7b05959fd87e3fc0cd33aeae19e242ac1
rl627 <rl627@drexel.edu>
2022-12-23 17:16:22 -0500

    praciting jinjas

---
+++ models/learning/jinja_test.sql
@@ -0,0 +1,45 @@
+{#-
+
+#### SETTING VARIABLES ####
+
+{% set my_str = "hi" %}
+{% set my_num =  3 %}
+{% set my_list = ['fox','bat','cat'] %}
+{% set temp = 45 %}
+
+#### Printing VARIABLES ####
+
+{{ my_str }}I want to write jingle for  {{ my_num }}  years!
+{{ my_list[0]}} is the first element in the list
+
+#### Looping  ####
+
+{% for animal in my_list %}
+   My favorite anmials is the {{ animal }}!!
+{% endfor %}
+
+
+#### Conditionals  ####
+
+{% if temp < 65 %}
+  Time for a late!
+{% else %}
+  Time for a swim!
+{% endif %}
+
+-#}
+#### Conditionals + loops together  ####
+
+{%- set foods = ['carrot', 'hotdog', 'cucumber'] -%}
+{%- for food in foods -%}
+  {%- if food == 'hotdog' -%}
+    {%- set food_type = 'snack' -%}
+  {%- else -%}
+    {%- set food_type = 'vegetable' -%}
+  {%- endif -%}
+  {{ food }} is a {{ food_type }}
+{% endfor %}
+
+
+
+      
