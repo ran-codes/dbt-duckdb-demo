0ca0a28cc56ef511ee3f31dfaa45d08d368a0897
rl627 <rl627@drexel.edu>
2023-01-03 10:26:37 -0500

    added documentation for age_recoding_general macro

--- models/learning/macros/insurance_macro_test.sql
+++ models/learning/macros/insurance_macro_test.sql
@@ -1,12 +1,5 @@
 {{ config(materialized='external', format = 'csv') }}
 SELECT 
-  age, 
-  CASE
-    WHEN (age <= 20.0) THEN ('0-20')
-    WHEN (age <= 60.0) THEN ('21-60')
-    ELSE ('60+')
-  END AS age_grp, 
-  region,
-  smoker, 
-  charges
+  *, 
+  {{recode_age_general_group('age')}} as age_grp,
 FROM {{ source('external_csv', 'insurance3r2') }}
