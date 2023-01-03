bbaa2fbbbc2346b01555f7679ec5993d03cd5c85
rl627 <rl627@drexel.edu>
2023-01-03 09:26:06 -0500

    cents to dollars macro: added rounding parameter and white space removal

--- macros/learning/cents_to_dollars.sql
+++ macros/learning/cents_to_dollars.sql
@@ -1,3 +1,3 @@
-{% macro cents_to_dollars(col_name) %}
-  {{col_name}}/100
+{%- macro cents_to_dollars(col_name, decimal_places = 2) -%}
+  round(1.0 * {{col_name}} / 100, {{decimal_places}} )
 {% endmacro %}
